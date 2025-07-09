ab3cb60beace378b9410c1f739e775b880069a18
Virendra-04 <vickygodse01@gmail.com>
2025-07-09 20:18:55 +0530

    DAY1

---
+++ Day1/src/day1/Day1.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Day1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Jay Ganesh"+" ");
+		System.out.println(123);
+	}
+
+}
---
+++ Day1/src/primitivedatatype/Day2.java
@@ -0,0 +1,32 @@
+package primitivedatatype;
+
+public class Day2 
+{
+	public static void main(String[] args) 
+	{
+		//Declaration
+		byte a;
+		//Initialization
+		a = 127;
+		System.out.println(a);
+		
+		short s;
+		s = 32694;
+		System.out.println(s);
+		
+		float f = 32.323255423f;
+		System.out.println(f);
+		
+		double d = 32.45645344d;
+		System.out.println(d);
+		
+		long l=8975749685l;
+		System.out.println(l);
+		
+		char c = 'a';
+		System.out.println(c);
+
+		
+	}
+
+}
